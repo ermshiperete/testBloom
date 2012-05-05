d0652e5b12d423fb38b7e63114c40a6008bb1d88
John Hatton <hattonjohn@gmail.com>
2012-05-05 08:43:24 -0600

    More Internationalization work

--- src/BloomExe/Edit/ConfigurationDialog.Designer.cs
+++ src/BloomExe/Edit/ConfigurationDialog.Designer.cs
@@ -28,9 +28,12 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this._browser = new Bloom.Browser();
 			this._cancelButton = new System.Windows.Forms.Button();
 			this._okButton = new System.Windows.Forms.Button();
+			this.localizationExtender1 = new Localization.UI.LocalizationExtender(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.localizationExtender1)).BeginInit();
 			this.SuspendLayout();
 			//
 			// _browser
@@ -39,6 +42,9 @@
 			| System.Windows.Forms.AnchorStyles.Left)
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this._browser.BackColor = System.Drawing.Color.DarkGray;
+			this.localizationExtender1.SetLocalizableToolTip(this._browser, null);
+			this.localizationExtender1.SetLocalizationComment(this._browser, null);
+			this.localizationExtender1.SetLocalizingId(this._browser, "ConfigurationDialog.Browser");
 			this._browser.Location = new System.Drawing.Point(-1, 0);
 			this._browser.Name = "_browser";
 			this._browser.Size = new System.Drawing.Size(670, 438);
@@ -48,6 +54,9 @@
 			//
 			this._cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this._cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.localizationExtender1.SetLocalizableToolTip(this._cancelButton, null);
+			this.localizationExtender1.SetLocalizationComment(this._cancelButton, null);
+			this.localizationExtender1.SetLocalizingId(this._cancelButton, "Common.CancelButton");
 			this._cancelButton.Location = new System.Drawing.Point(575, 447);
 			this._cancelButton.Name = "_cancelButton";
 			this._cancelButton.Size = new System.Drawing.Size(75, 23);
@@ -59,6 +68,9 @@
 			// _okButton
 			//
 			this._okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.localizationExtender1.SetLocalizableToolTip(this._okButton, null);
+			this.localizationExtender1.SetLocalizationComment(this._okButton, null);
+			this.localizationExtender1.SetLocalizingId(this._okButton, "Common.OkButton");
 			this._okButton.Location = new System.Drawing.Point(484, 447);
 			this._okButton.Name = "_okButton";
 			this._okButton.Size = new System.Drawing.Size(75, 23);
@@ -66,6 +78,10 @@
 			this._okButton.Text = "&OK";
 			this._okButton.UseVisualStyleBackColor = true;
 			this._okButton.Click += new System.EventHandler(this._okButton_Click);
+			//
+			// localizationExtender1
+			//
+			this.localizationExtender1.LocalizationManagerId = "Bloom";
 			//
 			// ConfigurationDialog
 			//
@@ -77,6 +93,10 @@
 			this.Controls.Add(this._okButton);
 			this.Controls.Add(this._cancelButton);
 			this.Controls.Add(this._browser);
+			this.localizationExtender1.SetLocalizableToolTip(this, null);
+			this.localizationExtender1.SetLocalizationComment(this, "This is the dialog used to set up the wall calendar");
+			this.localizationExtender1.SetLocalizationPriority(this, Localization.LocalizationPriority.Medium);
+			this.localizationExtender1.SetLocalizingId(this, "DynamicBookConfigurationDialog.WindowTitle");
 			this.MinimizeBox = false;
 			this.Name = "ConfigurationDialog";
 			this.ShowIcon = false;
@@ -84,6 +104,7 @@
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Setup";
 			this.Load += new System.EventHandler(this.ConfigurationDialog_Load);
+			((System.ComponentModel.ISupportInitialize)(this.localizationExtender1)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -93,5 +114,6 @@
 		private Browser _browser;
 		private System.Windows.Forms.Button _cancelButton;
 		private System.Windows.Forms.Button _okButton;
+		private Localization.UI.LocalizationExtender localizationExtender1;
 	}
 }
