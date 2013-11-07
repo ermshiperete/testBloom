cd9bbbd35ecca40ea637412bbf496996d58566a8
Eberhard Beilharz <eb1@sil.org>
2013-11-07 16:21:14 +0100

    New library

---
+++ MyLib/MyClass.cs
@@ -0,0 +1,14 @@
+// Copyright (c) 2013, SIL International.
+// Distributable under the terms of the MIT license (http://opensource.org/licenses/MIT).
+using System;
+
+namespace MyLib
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
